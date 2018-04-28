64d94194d6d6db9bbd582e6d0c5e066b58c9c1ca
tommyod <tommy.odland@gmail.com>
2018-04-28 11:49:42 +0200

    Kjørte ut nye filer, oppdaterte index.html

--- scripts/kopier_filer.py
+++ scripts/kopier_filer.py
@@ -6,6 +6,10 @@ Kopierer alle .pdf filer som matcher følgende mønster:
 (FAGKODE)_(2 TALL)_(V eller H)_(1-3 bokstaver)_(.pdf)
 
 Til en egen mappe.
+
+
+
+Oppdaterer også /docs index siden.
 @author: tommy
 """
 
@@ -13,6 +17,7 @@ import os
 import re
 import shutil
 import zipfile
+import more_itertools
 
 
 if __name__ == '__main__':
@@ -54,7 +59,7 @@ if __name__ == '__main__':
             print('Copied to:', copy_to)
             
     # Create a new zip file
-    file_name = os.path.join(path_start, PDF_DIR, 'alle.zip')
+    file_name = os.path.join(path_start, PDF_DIR, 'vgs_eksamener.zip')
     file_obj = zipfile.ZipFile(file_name, 'w', zipfile.ZIP_DEFLATED)
     with file_obj as open_zip_file:
         
@@ -72,6 +77,44 @@ if __name__ == '__main__':
             open_zip_file.write(file_to_zip, 
                                 arcname = os.path.basename(file_to_zip))
             print('Wrote to .zip file:', os.path.basename(file_to_zip))
+            
+            
+    # -------------------------------------------------------------------------
+    github_repo_url = r'https://github.com/matematikk/vgs_eksamener/blob/master/alle_pdf_filer'
+    template_path = os.path.join(path_here, '..', 'docs', 'index_template.html')
+    out_path = os.path.join(path_here, '..', 'docs', 'index.html')
+    with open(template_path, 'r') as html_template:
+        html_file = ''.join(l for l in html_template)
+    
+        for fagkode in FAGKODER:
+            html_code = '<ul>'
+            print(fagkode)
+            
+            relevant_files = os.listdir(os.path.join(path_start, PDF_DIR))
+            relevant_files = sorted((f for f in relevant_files if fagkode in f),
+                                    reverse=True)
+            
+            for lf, problem in more_itertools.chunked(relevant_files, 2):
+                link_name, _ = os.path.splitext(problem)
+                print(os.path.splitext(lf))
+                print(lf, problem)
+                html_code += f'<li><a href="{github_repo_url}/{problem}" target="_blank">{link_name}</a>'
+                html_code += f' [<a href="{github_repo_url}/{lf}" target="_blank">løsning</a>]</li>\n'
+            
+            html_file = html_file.replace(f'CONTENT_{fagkode}', html_code + '</ul>')
+            
+            print(html_file)
+        with open(out_path, 'w') as out_file:
+            out_file.write(html_file)
+        
+                
+                #<a href="https://github.com/tommyod/matte_eksamener_VGS/blob/master/alle_pdf_filer/S2_15H_lf.pdf" target="_blank">S2_15H_lf.pdf</a>
+            #for file in relevant_files:
+            #    print(' ', file)
+    print(html_code)
+            
+            
+        
     
     
     
